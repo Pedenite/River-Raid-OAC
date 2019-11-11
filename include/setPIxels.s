bb05ce340318d1092f9db2f1faaea46817ba1150
Pedenite <pedenite@gmail.com>
2019-11-10 22:00:53 -0300

    movimentação do aviao

--- include/setPIxels.s
+++ include/setPIxels.s
@@ -9,18 +9,18 @@
 .text
 SetPixels:	li t0, 0xff000000
 		li t1, 320
-		mul a1, a1, t1
-		add t0, t0, a1
-		add t0, t0, a2
-		lw t2, 0(a0)		# largura
-		lw t3, 4(a0)		# altura
-		mul t1, t1, t3
+		mul a1, a1, t1		# Calcula a altura
+		add t0, t0, a1		# Soma altura ao endereço inicial
+		add t0, t0, a2		# Soma posição ao endereço inicial
+		lw t2, 0(a0)		# largura do objeto
+		lw t3, 4(a0)		# altura do objeto
+		mul t1, t1, t3		
 		add t1, t1, t0		# Calcula endereço final da imagem
 		add t4, t2, t0		# Calcula endereço final da linha
-		addi a0, a0, 8
+		addi a0, a0, 8		# Pega o primeiro pixel da figura
 LOOP1:		bge t0, t1, FORA1
 LOOP2:		beq t0, t4, FORA2
-		lb t3, 0(a0)
+		lbu t3, 0(a0)
 		sb t3, 0(t0)
 		addi t0, t0, 1
 		addi a0, a0, 1
--- riverRaid.s
+++ riverRaid.s
@@ -1,4 +1,4 @@
-.eqv Water 0x0d2	# 210, aviao = 7f 127
+.eqv Water 0x0d1	# 209, aviao = 7f 127
 .eqv Land 0x063
 
 .data
@@ -6,21 +6,44 @@
 
 .text
 MAIN:
+	la tp,exceptionHandling	# carrega em tp o endereço base das rotinas do sistema ECALL
+ 	csrrw zero,5,tp 	# seta utvec (reg 5) para o endereço tp
+ 	csrrsi zero,0,1 	# seta o bit de habilitação de interrupção em ustatus (reg 0)	
+	
 	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
 	li t2,0xFF012C00	# endereco final 
-	li t3,0xd2d2d2d2		# cor 
+	li t3,0xd1d1d1d1	# cor
 LOOP: 	beq t1,t2,FORA		# Se for o último endereço então sai do loop
 	sw t3,0(t1)		# escreve a word na memória VGA
 	addi t1,t1,4		# soma 4 ao endereço
 	j LOOP			# volta a verificar
 FORA:	
+	li s0, 97		# ascii a
+	li s1, 100		# ascii d
+	li s2, 0		# deslocamento do aviao
+
+GAMELOOP:
 	la a0, plane
-	li a1, 200
-	li a2, 160
-	jal SetPixels
+	li a1, 200		# altura
+	li a2, 160		# posição 
+	add a2, a2, s2		# deslocamento
+	bne t2, s0, PULA1
+	addi s2, s2, -1
+PULA1:	bne t2, s1, PULA2
+	addi s2, s2, 1
+PULA2:	jal SetPixels
+	### Apenas verifica se há tecla pressionada
+KEY2:	li t1,0xFF200000		# carrega o endereço de controle do KDMMIO
+	lw t0,0(t1)			# Le bit de Controle Teclado
+	andi t0,t0,0x0001		# mascara o bit menos significativo
+   	beq t0,zero,FIM   	   	# Se não há tecla pressionada então vai para FIM
+  	lw t2,4(t1)  			# le o valor da tecla tecla
+	sw t2,12(t1)  			# escreve a tecla pressionada no display
+FIM:					
+	#li a0,10   
+	#li a7,132
+	#ecall
+j GAMELOOP
 
-li a7, 10
-ecall
-
-.include "include/SYSTEMv17.s"
 .include "include/SetPixels.s"
+.include "include/SYSTEMv17.s"
