b5f5107bd2fe8b41393b1df93a28027cbdc9fbba
Pedenite <pedenite@gmail.com>
2019-12-07 19:15:15 -0300

    correção de bugs

--- include/controlaVida.s
+++ include/controlaVida.s
@@ -7,8 +7,10 @@
 .text
 ControlaVida:	li t0, 10000
 		remu t0, s5, t0
+		beq s5, zero, ContinuaVida
 		beq t0, zero, VidaExtra	# ganha vida extra a cada 10000 pontos
-ContinuaVida:	li t0, 64		# limite esquerda
+ContinuaVida:	addi a2, s2, 160
+		li t0, 64		# limite esquerda
 		li t1, 256		# limite direita
 		blt a2, t0, MORTE	# morte por parede esquerda
 		addi a2, a2, 14		# largura do avião
@@ -18,8 +20,8 @@ AINDAVIVO:	ret
 MORTE:		addi s4, s4, -1
 		j GAME_OVER
 		
-VidaExtra:	li t0, 3
-		beq s4, t0, ContinuaVida	# máximo de vidas = 3
+VidaExtra:	li t0, 4
+		bge s4, t0, ContinuaVida	# máximo de vidas = 3 + vida 0
 		addi s4, s4, 1
 		j ContinuaVida
 		
